f2c69afb54f7ea69f7364ea9586996f967ae4290
Dipesh Kumar Gupta <144589068+dkgtech@users.noreply.github.com>
2024-12-01 17:16:17 +0530

    Identity Operators

---
+++ 10_IdentityOperators.py
@@ -0,0 +1,15 @@
+# Identity operators are used to compare items to see if they are the same object with the same memory address.
+#  is, is not
+
+a = 123
+b = "1234"
+
+print(a is b)
+print (a is not b)
+
+
+x = 123
+y = 456
+
+print(x is y)
+print(x is not y)
