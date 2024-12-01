84562a09e28b3702dbc604d644d9f9262576f890
Dipesh Kumar Gupta <144589068+dkgtech@users.noreply.github.com>
2024-12-01 17:43:54 +0530

    Bitwise Operators

---
+++ 11_BitwiseOperators.py
@@ -0,0 +1,26 @@
+'''
+1. AND (&)
+2. OR (|)
+3. XOR (^)
+4. << zero fill left shift
+5. >> zero fill right shift
+
+'''
+
+print(bin(10))
+print(bin(15))
+
+# and
+print(10 & 8)
+
+# or
+print(10 | 8)
+
+# xor
+print(10 ^ 8)
+
+# right shift
+print(10>>2)
+
+# left shift
+print(10<<2)
