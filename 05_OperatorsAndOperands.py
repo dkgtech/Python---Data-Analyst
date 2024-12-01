3e1272da4f788e64ac496c13ba2f0029400f054a
Dipesh Kumar Gupta <144589068+dkgtech@users.noreply.github.com>
2024-12-01 14:10:44 +0530

    Operators and Operands file created

---
+++ 05_OperatorsAndOperands.py
@@ -0,0 +1,13 @@
+# Operators indicate what operation is to be performed while Operands indicates on what the action or the operation should be performed.
+
+# x+y = 1, here x & y are operands & + is operator
+
+# type of operators
+
+# 1. Arithmetic operators
+# 2. Comparison operators
+# 3. Logical operators
+# 4. Assignment operators
+# 5. Identify operators
+# 6. Membership operators
+# 7. Bitwise operators
