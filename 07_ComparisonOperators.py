4a1105f495b7de3de87cff6fb49a7276cb182fed
Dipesh Kumar Gupta <144589068+dkgtech@users.noreply.github.com>
2024-12-01 14:27:09 +0530

    Comparsion Operators

---
+++ 07_ComparisonOperators.py
@@ -0,0 +1,28 @@
+'''
+
+1. Less Than <
+2. Greater Than >
+3. Equal to ==
+4. Not Equal to !=
+5. Greater Than Equal to >=
+6. Less Than Equal to <=
+
+'''
+
+# Less Than
+print(3<2)
+
+# Greater Than
+print(3>2)
+
+#Equal to
+print(3==3)
+
+# Not Equal to
+print(3!=3)
+
+# Greater Than Equal to
+print(3>=3)
+
+# Less Than Equal to
+print(3<=2)
