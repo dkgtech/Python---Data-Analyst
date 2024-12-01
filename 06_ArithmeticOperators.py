2873e7660b673d185eec69bc5ccd004012ed259f
Dipesh Kumar Gupta <144589068+dkgtech@users.noreply.github.com>
2024-12-01 14:19:55 +0530

    Arithmetic Operators

---
+++ 06_ArithmeticOperators.py
@@ -0,0 +1,19 @@
+'''
+1. Additon +
+2. Subtraction -
+3. Multiplication *
+4. Floor Division //
+5. Moduls %
+6. Division /
+7. Exponentiation **
+
+'''
+
+# moduls - Remainder
+print(9%2)
+
+# floor division - Quotient
+print(8//3)
+
+# exponentitation - Power
+print(2**10)
