27b49dbd69a338efadeec8c1db1693e1351eb44f
Dipesh Kumar Gupta <144589068+dkgtech@users.noreply.github.com>
2024-12-01 14:04:16 +0530

    Problem Solving file added & file name changed for type casting

---
+++ 04_ProblemSolving.py
@@ -0,0 +1,56 @@
+# write a program to print the below details
+
+name="Dipesh"
+age = 23
+address ="Mumbai,India"
+print(name)
+print(age)
+print(address)
+
+
+# swapping two variables
+
+a = 10
+b = 20
+temp = b
+b=a
+a=temp
+print("The value of a is ",a )
+print("The value of b is ",b)
+
+x=1
+y=2
+x,y = y,x
+print("The value of x is ",x)
+print("The value of y is",y)
+
+# write a program to convert a float into interger.
+
+pi = 3.14
+pi = int(pi)
+print(type(pi))
+print(pi)
+
+# write a program to take details from students for ID card & then print it in different lines
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+email = input("Enter your email id: ")
+grade = input("Enter your class: ")
+phone = input("Enter your phone number: ")
+
+print("Student Identity Card")
+
+print("Name: ",name)
+print("Age: ",age)
+print("Email: ",email)
+print("Class: ",grade)
+print("Phone: ",phone)
+
+# write a program to take an user input as integer & then convert it to float
+
+marks = int(input("Enter your marks: "))
+marks = float(marks)
+print("Your marks :",marks)
+print(type(marks))
+
