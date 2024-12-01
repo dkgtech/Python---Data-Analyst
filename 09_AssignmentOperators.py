a3fcbe217ed64f60a03cb85b32f33f6cb59e1d47
Dipesh Kumar Gupta <144589068+dkgtech@users.noreply.github.com>
2024-12-01 17:11:21 +0530

    Assignment Operators

---
+++ 09_AssignmentOperators.py
@@ -0,0 +1,13 @@
+# =, +=, -=, *=
+
+score = 0
+print("Initially the value of score is ",score)
+
+score+=1
+print("After adding 1 to score , the value of score is ",score)
+
+score -=1
+print("After subtracting 1 from score, the value of score is ",score)
+
+score*=3
+print("After multiplying 3 to the score, the value of score is ",score)
