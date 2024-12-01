ee4c0df5a793bd702181bb03b90850c54a4c6627
Dipesh Kumar Gupta <144589068+dkgtech@users.noreply.github.com>
2024-12-01 17:48:30 +0530

    Membership Operators

---
+++ 12_MembershipOperators.py
@@ -0,0 +1,12 @@
+# Membership operators are used to check the presence of a sequence in an object & it is case-sensitive
+# in
+# not in
+
+# in
+name = "Dipesh"
+print("D" in name)
+print("a" in name)
+
+# not in
+print("D" not in name)
+print("a" not in name)
